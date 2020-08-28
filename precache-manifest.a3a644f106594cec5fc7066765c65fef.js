f0bac1a9a159e5629c6340e0c02c5047dc092638
Aditya Pranav Bhuvanapalli <pranavba99@gmail.com>
2020-08-29 02:24:32 +0530

    Updates

---
+++ precache-manifest.a3a644f106594cec5fc7066765c65fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fb774a9a797ae52dc10",
+    "url": "/to-do-tracker/static/css/main.67d7d98a.chunk.css"
+  },
+  {
+    "revision": "5fb774a9a797ae52dc10",
+    "url": "/to-do-tracker/static/js/main.c28faa22.chunk.js"
+  },
+  {
+    "revision": "cf6f01fd1904c8618dfc",
+    "url": "/to-do-tracker/static/js/runtime~main.3ac2c4c9.js"
+  },
+  {
+    "revision": "a5d74f29ff2227464bfb",
+    "url": "/to-do-tracker/static/js/2.3c9ed73b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-tracker/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8068f224484a93f970779fd1b81a9266",
+    "url": "/to-do-tracker/index.html"
+  }
+];
