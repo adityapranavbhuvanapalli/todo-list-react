38eca8b60fb312eb19c66dfc154ddf176b8ffd72
Aditya Pranav Bhuvanapalli <pranavba99@gmail.com>
2020-08-31 18:04:42 +0530

    update

---
+++ build/precache-manifest.6e15a9b8619240d2380d391244f9ec07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e77255b2b2b6bfa3313d",
+    "url": "/todo-list-react/static/css/main.96086c72.chunk.css"
+  },
+  {
+    "revision": "e77255b2b2b6bfa3313d",
+    "url": "/todo-list-react/static/js/main.0701f2de.chunk.js"
+  },
+  {
+    "revision": "35e3544cd546257cfadf",
+    "url": "/todo-list-react/static/js/runtime~main.bb4d8c92.js"
+  },
+  {
+    "revision": "ff54ec0d889e82dbfd45",
+    "url": "/todo-list-react/static/js/2.2faa3a81.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/todo-list-react/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "073e5a6715e7493b725bcf021b4ee9ab",
+    "url": "/todo-list-react/index.html"
+  }
+];
